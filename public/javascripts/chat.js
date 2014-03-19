7c0c23569062c58bb39ec00b4f60ef18a7d06d27
Nate Watson <meromorphic@gmail.com>
2014-03-18 17:39:05 -0700

    multi-room support, Hotel California style

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -2,8 +2,12 @@ var ChatApp = window.ChatApp = window.ChatApp || {};
 
 ChatApp.Chat = function(socket){
   this.socket = socket;
+  this.currentRoom = "lobby";
 }
 
 ChatApp.Chat.prototype.sendMessage = function(text){
-  this.socket.emit("message", {text: text});
+  this.socket.emit("message", {
+    text: text,
+    room: this.currentRoom
+  });
 }
--- public/javascripts/chat_ui.js
+++ public/javascripts/chat_ui.js
@@ -2,11 +2,12 @@ var ChatApp = window.ChatApp = window.ChatApp || {};
 
 ChatApp.ChatUI = function(){
   $("#chat-form").on("submit", this.sendChat);
+  $("#tabs").on("click", "button", this.displayRoom);
   var socket = io.connect();
   ChatApp.ChatInstance = new ChatApp.Chat(socket);
   socket.on("message", this.receiveMessage);
   socket.on("nicknameChangeResult", this.receiveNicknameChangeResult);
-  socket.on("welcome", this.populateNicknames);
+  socket.on("welcome", this.initRoom);
   socket.on("userLeave", this.removeNickname);
   socket.on("userJoin", this.addNickname);
 }
@@ -20,38 +21,66 @@ ChatApp.ChatUI.prototype.sendChat = function(event){
 }
 
 ChatApp.ChatUI.prototype.receiveMessage = function(data) {
-  $("#chats").append("<li>" + data["text"] + "</li>");
+  $("#chats-" + data["room"]).append("<li>" + data["text"] + "</li>");
 }
 
 ChatApp.ChatUI.prototype.receiveNicknameChangeResult = function(data){
+  console.log(data["room"]);
   if (data["success"]){
-    var oldNameContainer = $("#users").find("li#" + data["oldName"]);
+    var oldNameContainer = $("#users-" + data["room"]).find("li#" + data["oldName"]);
     oldNameContainer.remove();
 
-    $("#users").append("<li id=" + data["newName"] + ">" + data["newName"] + "</li>");
-    $("#chats").append("<li>" + "User " + data["oldName"] + " has changed nicks to " + data["newName"]);
+    $("#users-" + data["room"]).append("<li id=" + data["newName"] + ">" + data["newName"] + "</li>");
+    $("#chats-" + data["room"]).append("<li>" + "User " + data["oldName"] + " has changed nicks to " + data["newName"]);
   } else {
-    $("#chats").append("<li>" + data["text"] + "</li>");
+    $("#chats-" + data["room"]).append("<li>" + data["text"] + "</li>");
   }
 }
 
-ChatApp.ChatUI.prototype.populateNicknames = function(data){
-  $("#users").empty();
-  data.nicknames.forEach(function(nickname){
-    $("#users").append("<li id=" + nickname + ">" + nickname + "</li>");
+ChatApp.ChatUI.prototype.displayRoom = function(event){
+  $("h1").text($(event.target).data("room"));
+
+  $("#users").children().each(function(index, roomUsers) {
+    if ($(roomUsers).data("room") === $(event.target).data("room")) {
+      $(roomUsers).removeClass("hidden");
+    } else {
+      $(roomUsers).addClass("hidden");
+    }
   });
-  $("h1").text(data["room"]);
-  $("#chats").empty();
+
+  $("#chats").children().each(function(index, roomChats) {
+    if ($(roomChats).data("room") === $(event.target).data("room")) {
+      $(roomChats).removeClass("hidden");
+    } else {
+      $(roomChats).addClass("hidden");
+    }
+  });
+  ChatApp.ChatInstance.currentRoom = $(event.target).data("room");
+}
+
+ChatApp.ChatUI.prototype.initRoom = function(data){
+  var usersContainer = $("<ul data-room='" + data["room"] + "' id=users-" + data["room"] + ">");
+  data.nicknames.forEach(function(nickname){
+    usersContainer.append("<li id=" + nickname + ">" + nickname + "</li>");
+  });
+  $("#users").append(usersContainer);
+
+  var chatContainer = $("<ul data-room='" + data["room"] + "' id=chats-" + data["room"] + ">");
+  $("#chats").append(chatContainer);
+
+  var roomTab = $("<button data-room='" + data["room"] + "'>" + data["room"] + "</button>");
+  $("#tabs").append(roomTab);
+
+  roomTab.trigger("click");
 }
 
 ChatApp.ChatUI.prototype.removeNickname = function(data){
-  var oldNameContainer = $("#users").find("li#" + data["nickname"]);
+  var oldNameContainer = $("#users-" + data["room"]).find("li#" + data["nickname"]);
   oldNameContainer.remove();
-  $("#chats").append("<li>" + data["nickname"] + " has left.</li>");
+  $("#chats-" + data["room"]).append("<li>" + data["nickname"] + " has left.</li>");
 }
 
 ChatApp.ChatUI.prototype.addNickname = function(data) {
-  console.log("addNickname fired");
-  $("#chats").append("<li>" + "User " + data["nickname"] + " has joined the room.");
-  $("#users").append("<li id=" + data["nickname"] + ">" + data["nickname"] + "</li>");
+  $("#chats-" + data["room"]).append("<li>" + "User " + data["nickname"] + " has joined the room.");
+  $("#users-" + data["room"]).append("<li id=" + data["nickname"] + ">" + data["nickname"] + "</li>");
 }
